ba8a7acc1891871dec99a5c6cb826535e3730269
= <carlosd.vargasvarela@gmail.com>
2025-08-22 10:42:37 -0600

    Se corrige JS

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,5 @@
-// app/javascript/application.js
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import * as bootstrap from "bootstrap"
-
-// Expón bootstrap globalmente para que esté disponible en Stimulus controllers
-
-
 document.addEventListener("turbo:load", function () {
     // Inicializa tooltips de Bootstrap (si usas tooltips)
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
@@ -14,3 +7,21 @@ document.addEventListener("turbo:load", function () {
         new bootstrap.Tooltip(tooltipTriggerEl)
     })
 });
+
+// Limpieza antes del cache de Turbo (evita estados pegados entre navegaciones)
+document.addEventListener("turbo:before-cache", () => {
+    // Cierra modales abiertos
+    document.querySelectorAll(".modal.show").forEach(el => {
+        const modalInstance = bootstrap.Modal.getInstance(el)
+        if (modalInstance) modalInstance.hide()
+    })
+
+    // Cierra dropdowns abiertos
+    document.querySelectorAll(".dropdown-menu.show").forEach(menu => {
+        const toggle = menu.previousElementSibling
+        if (toggle) {
+            const dropdownInstance = bootstrap.Dropdown.getInstance(toggle)
+            if (dropdownInstance) dropdownInstance.hide()
+        }
+    })
+})
