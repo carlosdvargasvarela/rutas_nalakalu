c48f098a267d9938b3c537b5c9be07c8a352b37d
= <carlosd.vargasvarela@gmail.com>
2025-08-22 10:34:12 -0600

    Se corrigen problemas p1

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,22 +5,12 @@ import "controllers"
 import * as bootstrap from "bootstrap"
 
 // Expón bootstrap globalmente para que esté disponible en Stimulus controllers
-window.bootstrap = bootstrap
 
-// Limpieza antes del cache de Turbo (evita estados pegados entre navegaciones)
-document.addEventListener("turbo:before-cache", () => {
-    // Cierra modales abiertos
-    document.querySelectorAll(".modal.show").forEach(el => {
-        const modalInstance = bootstrap.Modal.getInstance(el)
-        if (modalInstance) modalInstance.hide()
-    })
 
-    // Cierra dropdowns abiertos
-    document.querySelectorAll(".dropdown-menu.show").forEach(menu => {
-        const toggle = menu.previousElementSibling
-        if (toggle) {
-            const dropdownInstance = bootstrap.Dropdown.getInstance(toggle)
-            if (dropdownInstance) dropdownInstance.hide()
-        }
+document.addEventListener("turbo:load", function () {
+    // Inicializa tooltips de Bootstrap (si usas tooltips)
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+        new bootstrap.Tooltip(tooltipTriggerEl)
     })
-})
+});
--- config/importmap.rb
+++ config/importmap.rb
@@ -4,5 +4,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "sortablejs", to: "https://ga.jspm.io/npm:sortablejs@1.15.2/modular/sortable.core.esm.js"
-pin "bootstrap", integrity: "sha384-Hea0Yk7N2rQhmxzzIGikclw/jBEhpCDFFXi+rlgF1qZtC7eAazBGapuqKzAe6yXQ" # @5.3.7
-pin "@popperjs/core", to: "@popperjs--core.js", integrity: "sha384-bfekMOfeUlr1dHZfNaAFiuuOeD7r+Qh45AQ2HHJY7EAAI4QGJ6qx1Qq9gsbvS+60" # @2.11.8
+# pin "bootstrap", integrity: "sha384-Hea0Yk7N2rQhmxzzIGikclw/jBEhpCDFFXi+rlgF1qZtC7eAazBGapuqKzAe6yXQ" # @5.3.7
+# pin "@popperjs/core", to: "@popperjs--core.js", integrity: "sha384-bfekMOfeUlr1dHZfNaAFiuuOeD7r+Qh45AQ2HHJY7EAAI4QGJ6qx1Qq9gsbvS+60" # @2.11.8
