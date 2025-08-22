f6412006131c0d579950370d911ba33a3ac38190
= <carlosd.vargasvarela@gmail.com>
2025-08-22 09:33:15 -0600

    Se agregan cambios en dashboard

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -39,7 +39,11 @@ class Delivery < ApplicationRecord
   scope :service_cases, -> { where(delivery_type: [ :pickup, :return_delivery, :onsite_repair ]) }
   scope :normal_deliveries, -> { where(delivery_type: :normal) }
   scope :pending, -> { where(status: [ :scheduled, :ready_to_deliver, :in_route ]) }
-  scope :overdue, -> { where("delivery_date < ?", Date.current).where.not(status: [ :delivered, :rescheduled, :cancelled ]) }
+  scope :overdue, -> {
+  where("delivery_date < ?", Date.current)
+    .where.not(status: [ Delivery.statuses[:delivered],
+                         Delivery.statuses[:rescheduled],
+                         Delivery.statuses[:cancelled] ])}
 
   # Métodos de conveniencia
   def service_case?
