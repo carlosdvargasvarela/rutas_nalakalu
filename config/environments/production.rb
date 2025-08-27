7d949a1fafaeb12bca0e0fd7c177c3b28be8b5c1
= <carlosd.vargasvarela@gmail.com>
2025-08-27 12:11:18 -0600

    Se agrega mailer a production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,4 +105,20 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address:              "correo.nalakalu.com",
+    port:                 465,
+    domain:               "nalakalu.com",
+    user_name:            ENV["SMTP_USERNAME"],
+    password:             ENV["SMTP_PASSWORD"],
+    authentication:       :login,
+    ssl:                  true,
+    tls:                  true,
+    enable_starttls_auto: true
+  }
+
+  config.action_mailer.default_url_options = { host: "rutas-nalakalu.com", protocol: "https" }
 end
