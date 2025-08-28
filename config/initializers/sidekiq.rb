949998e571548effcdeb185ab31b835272fca950
= <carlosd.vargasvarela@gmail.com>
2025-08-28 14:38:54 -0600

    Se corrige errores con sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,17 +1,20 @@
-# config/initializers/sidekiq.rb
-redis_url = ENV.fetch('REDIS_URL', ENV.fetch('REDISCLOUD_URL', 'redis://localhost:6379/1'))
+# config/sidekiq.rb
+redis_url =
+  ENV["REDIS_TLS_URL"] || # Heroku puede darte este si tu plan lo expone
+  ENV["REDIS_URL"]      || # Heroku Redis mini
+  ENV["REDISCLOUD_URL"] || # RedisCloud fallback
+  "redis://localhost:6379/0" # Default para development
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: redis_url }
-
-  schedule_file = "config/sidekiq.yml"
-  if File.exist?(schedule_file) && Sidekiq.server?
-    Sidekiq::Scheduler.dynamic = true
-    Sidekiq.schedule = YAML.load_file(schedule_file)[:schedule]
-    Sidekiq::Scheduler.reload_schedule!
-  end
+  config.redis = {
+    url: redis_url,
+    ssl_params: (redis_url.start_with?("rediss://") ? { verify_mode: OpenSSL::SSL::VERIFY_NONE } : {})
+  }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: redis_url }
+  config.redis = {
+    url: redis_url,
+    ssl_params: (redis_url.start_with?("rediss://") ? { verify_mode: OpenSSL::SSL::VERIFY_NONE } : {})
+  }
 end
