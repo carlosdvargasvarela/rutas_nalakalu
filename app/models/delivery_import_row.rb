d035173d583bb2679ce10264f36c449c1601365f
= <carlosd.vargasvarela@gmail.com>
2025-08-28 15:23:02 -0600

    Fix row_errors serialization for Postgres

--- app/models/delivery_import_row.rb
+++ app/models/delivery_import_row.rb
@@ -1,10 +1,8 @@
 class DeliveryImportRow < ApplicationRecord
   belongs_to :delivery_import
 
-  # Para SQLite serializamos y parseamos manualmente
-  # En Postgres jsonb ya funciona nativo
-  if ActiveRecord::Base.connection.adapter_name.downcase.starts_with?("sqlite")
-    serialize :data, coder: JSON
-    serialize :row_errors, coder: JSON
-  end
+  # Serializamos siempre para que tanto SQLite como Postgres
+  # devuelvan arrays/hashes en lugar de strings
+  serialize :data, JSON
+  serialize :row_errors, JSON
 end
