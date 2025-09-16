780582bf68db8481c1d881b3b9eb136c91d40038
= <carlosd.vargasvarela@gmail.com>
2025-09-15 21:18:22 -0600

    Se agregan funcionalidades en rutas

--- db/migrate/20250915151419_fix_driver_sessions_foreign_keys.rb
+++
@@ -1,6 +0,0 @@
-class FixDriverSessionsForeignKeys < ActiveRecord::Migration[7.2]
-  def change
-    remove_foreign_key :driver_sessions, :delivery_plans
-    add_foreign_key :driver_sessions, :delivery_plans, on_delete: :cascade
-  end
-end
