ec1567c91caf7cb17120336b85205c4192b9e20c
= <carlosd.vargasvarela@gmail.com>
2025-09-28 23:10:59 -0600

    Se repara Bootstrap JS

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,21 +1,3 @@
 // app/javascript/application.js
 import "@hotwired/turbo-rails"
 import "controllers"
-import * as bootstrap from "bootstrap"
-
-window.bootstrap = bootstrap
-
-document.addEventListener("turbo:load", () => {
-    // Inicializar tooltips/dropdowns refrescados con Turbo
-    document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(el => {
-        if (!bootstrap.Tooltip.getInstance(el)) {
-            new bootstrap.Tooltip(el)
-        }
-    })
-
-    document.querySelectorAll('[data-bs-toggle="dropdown"]').forEach(el => {
-        if (!bootstrap.Dropdown.getInstance(el)) {
-            new bootstrap.Dropdown(el)
-        }
-    })
-})
--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,11 @@
 import { Application } from "@hotwired/stimulus"
+import * as bootstrap from "bootstrap"
 
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
+window.bootstrap = bootstrap
 window.Stimulus   = application
 
 export { application }
