c928c839e78463ef619492d31e6bbea8031bbc3c
Carlos Daniel Vargas Varela <carlosd.vargasvarela@gmail.com>
2026-05-04 01:09:32 -0600

    Integración de Quickbooks con gema QBWC, WORKERS

--- config/initializers/qbwc.rb
+++ config/initializers/qbwc.rb
@@ -1,14 +1,33 @@
 QBWC.configure do |c|
+  # Credenciales que pondrás en el archivo .qwc y en el Web Connector
   c.username = ENV.fetch("QB_USER", "admin")
   c.password = ENV.fetch("QB_PASS", "123456")
 
+  # Dejar vacío para usar el archivo que esté abierto en QuickBooks
   c.company_file_path = ""
+
+  # Versión QBXML compatible con QuickBooks Desktop 2024
   c.min_version = "13.0"
+
+  # Tipo de QuickBooks (QuickBooks Financial Software, no POS)
   c.api = :qb
+
+  # Almacenamiento en base de datos
   c.storage = :active_record
+
+  # URL de soporte
   c.support_site_url = "https://rutas-nalakalu.com"
+
+  # GUID único para identificar tu app ante QuickBooks
   c.owner_id = "{57F3B9B1-86F1-4fcc-B1EE-566DE1813D20}"
+
+  # nil = solo corre manualmente desde el Web Connector (recomendado para pruebas)
   c.minutes_to_run = nil
+
+  # En caso de error, detener el proceso
   c.on_error = :stop
+
   c.logger = Rails.logger
+
+  c.parser = :rexml
 end
