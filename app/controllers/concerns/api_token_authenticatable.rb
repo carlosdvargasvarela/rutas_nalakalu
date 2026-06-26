0437ca3e35845bed5011330e026e03ae203f199c
Carlos Daniel Vargas Varela <carlosd.vargasvarela@gmail.com>
2026-06-26 16:45:40 -0600

    feat: add api_token to users + ApiTokenAuthenticatable concern

---
+++ app/controllers/concerns/api_token_authenticatable.rb
@@ -0,0 +1,19 @@
+module ApiTokenAuthenticatable
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :authenticate_api_token!
+  end
+
+  private
+
+  def authenticate_api_token!
+    token = request.headers["X-Driver-Token"]
+    @current_api_user = User.find_by(api_token: token)
+    render json: { error: "No autorizado" }, status: :unauthorized unless @current_api_user
+  end
+
+  def current_user
+    @current_api_user
+  end
+end
